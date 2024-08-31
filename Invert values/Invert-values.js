b896fa832477d14a7e1b3ed775c2f1573f8f937e
Narek5412 <narek.harutyunyan.2001.09@gmail.com>
2024-08-31 13:02:53 +0400

    new task

---
+++ Invert values/Invert-values.js
@@ -0,0 +1,10 @@
+/*Given a set of numbers, return the additive inverse of each.
+Each positive becomes negatives, and the negatives become positives.
+*/
+function invert(array) {
+
+    for(let i = 0; i < array.length; i++){
+       array[i] *= -1;
+    }return array;
+    // return array.map(i => 0 - i);
+}console.log(invert([1,2,3,4,5,6,7,8,9,10]));

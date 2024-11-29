cb5492e702a4ebee75260a35926a8dabcb7d418a
Narek5412 <narek.harutyunyan.2001.09@gmail.com>
2024-11-29 12:49:55 +0400

    new task

---
+++  Reverse or rotate?/Reverse-or-rotate.js
@@ -0,0 +1,49 @@
+/**The input is a string str of digits. Cut the string into chunks (a chunk here is a substring of the initial string)
+ of size sz (ignore the last chunk if its size is less than sz).
+ If the sum of a chunk's digits is divisible by 2, reverse that chunk; otherwise rotate it to the left by one position.
+ Put together these modified chunks and return the result as a string.
+ If
+ sz is <= 0 or if str == "" return ""
+ sz is greater (>) than the length of str it is impossible to take a chunk of size sz hence return "".
+ Examples:
+ ("123456987654", 6) --> "234561876549"
+ ("123456987653", 6) --> "234561356789"
+ ("66443875", 4) --> "44668753"
+ ("66443875", 8) --> "64438756"
+ ("664438769", 8) --> "67834466"
+ ("123456779", 8) --> "23456771"
+ ("", 8) --> ""
+ ("123456779", 0) --> ""
+ ("563000655734469485", 4) --> "0365065073456944"
+ Example of a string rotated to the left by one position:
+ s = "123456" gives "234561".
+ */
+function revrot(str, sz) {
+    if(str === ""|| sz > str.length|| sz<= 0){
+        return ""
+    }
+    let result = ""
+    let countOfLeft = 0
+    let countOfRigth = 0
+    let leftPart = str.slice(0,sz)
+    let rigthPart = str.slice(sz,str.length)
+    for(let i =0;i<leftPart.length;i++){
+        if(+leftPart[i] % 2 === 1){
+            countOfLeft++
+        }
+    }if(countOfLeft % 2 === 1){
+        leftPart = leftPart.slice(1,leftPart.length) + leftPart[0]
+    }else{
+        leftPart = leftPart.split("").reverse().join("")
+    }
+    for(let i =0;i<rigthPart.length;i++){
+        if(+rigthPart[i] % 2 === 1){
+            countOfRigth++
+        }
+    }if(countOfRigth % 2 === 1){
+        rigthPart = rigthPart.slice(1,rigthPart.length) + rigthPart[0]
+    }else{
+        rigthPart = rigthPart.split("").reverse().join("")
+    }return leftPart + rigthPart
+}console.log(revrot("123456779", 8))
+console.log(revrot("733049910872815764",5))

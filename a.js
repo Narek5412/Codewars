1446fa094c206cb359a95ef16c6b007a8d2aed3b
Narek5412 <narek.harutyunyan.2001.09@gmail.com>
2024-11-29 13:40:20 +0400

    new task

--- a.js
+++
@@ -1,6 +0,0 @@
-function isPrimeNumber(number) {
-    let result = null;
-    for (i = 2; i <= number/2; i++) {
-        number % i === 0 ? result = false break : result = true
-    } return result ;
-}console.log(isPrimeNumber(101))
